475b924ef66fcb46eedc97384a9fbbb9e8898f37
Shrish Apte <shrish.apte@gmail.com>
2017-06-08 15:26:32 -0400

    Initial Commit

---
+++ ResearchKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResearchKitDemo
+//
+//  Created by LION-2 on 6/8/17.
+//  Copyright © 2017 Infosys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
